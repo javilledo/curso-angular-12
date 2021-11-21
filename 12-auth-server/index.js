c22006430401adec7f5951f04d50f9e46be0b524
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2021-11-21 09:53:44 +0100

    Despliegue

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -22,6 +23,16 @@ app.use( express.json() );
 
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
+
+// Manejar demás rutas
+// (Al copiar el build de Angular en public de este backend, Angular solo maneja la principal, pero al recargar
+// o ir a otra ruta válida en Angular pero no en el backend, trata de servir el resultado del servidor GET o POST)
+// Para eso se incluye useHash:true en el app-routing, para que sean compatible las rutas de Angular y backend
+// Lo anterior incluye un # intermedio para diferenciar las rutas de Angular de las del backend
+app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
+
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
